48c667d1fd089c3250167fc901a084d56a9b5d8d
Tarun Angrish <angrish.t@northeastern.edu>
2024-02-08 14:45:20 -0500

    Queue and Stack Implementation using LL

--- src/main/java/Stack/StackUsingLL.java
+++ src/main/java/Stack/StackUsingLL.java
@@ -1,4 +1,65 @@
 package Stack;
 
-public class StackUsingLL {
+import java.util.Iterator;
+
+public class StackUsingLL<Item> implements Iterable<Item>{
+    private Node first; //top of stack, most recently added node
+    private int N; // no of times
+
+    private class Node{
+        Item item;
+        Node next;
+    }
+
+    public boolean isEmpty(){
+        return first == null;
+    }
+
+    public int size(){
+        return N;
+    }
+
+    public void push(Item item){
+        Node oldFirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldFirst;
+        N++;
+    }
+
+    public Item pop(){
+        Item item = first.item;
+        first = first.next;
+        N--;
+        return item;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item>{
+        private Node current = first;
+
+        public boolean hasNext(){
+            return current!=null;
+        }
+        public void remove(){}
+        public Item next(){
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+
+    public static void main(String[] args){
+        StackUsingLL stack = new StackUsingLL();
+        stack.push(2);
+        stack.push(1);
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+
+    }
+
 }
